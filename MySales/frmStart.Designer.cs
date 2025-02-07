d471f134334fe45ea0b6e153b640afbde4190bfe
elfath-store <elfath-store@DESKTOP-04E9FQ9>
2025-02-07 19:21:34 +0200

    Add project files.

---
+++ MySales/frmStart.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace MySales
+{
+    partial class frmStart
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // frmStart
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(133, 120);
+            ControlBox = false;
+            FormBorderStyle = FormBorderStyle.None;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmStart";
+            Opacity = 0D;
+            ShowInTaskbar = false;
+            Load += this.frmStart_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ MySales/frmStart.cs
@@ -0,0 +1,15 @@
+namespace MySales
+{
+    public partial class frmStart : Form
+    {
+        public frmStart()
+        {
+            InitializeComponent();
+        }
+
+        private void frmStart_Load(object sender, EventArgs e) 
+        { 
+        
+        }
+    }
+}
